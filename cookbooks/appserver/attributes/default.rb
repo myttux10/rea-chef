7c99597b9d9413f8c13dc46531b924f27cccd981
mbh <myttux10@gmail.com>
2016-01-13 21:27:15 +0930

    config changes to install ruby using rvm

--- cookbooks/appserver/attributes/default.rb
+++ cookbooks/appserver/attributes/default.rb
@@ -1,4 +1,7 @@
 default[:appserver][:rvm][:version]="1.26.11"
-default[:appserver][:ruby][:version]=["2.1.8","2.2.2"]
-default[:appserver][:ruby][:default_version]="2.1.8"
+default[:appserver][:ruby][:version]=["2.2.2"]
+default[:appserver][:ruby][:default_version]="2.2.2"
 default[:appserver][:unicorn][:version]="5.0.0"
+default[:appserver][:app_user]="sapp"
+default[:appserver][:app_base_dir]="/var/www/apps"
+
