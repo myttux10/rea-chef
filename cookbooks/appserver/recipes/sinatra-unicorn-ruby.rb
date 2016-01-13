d74435afde74c69548213562433de932b0b27d6c
mbh <myttux10@gmail.com>
2016-01-13 21:29:52 +0930

    setup unicorn service

--- cookbooks/appserver/recipes/sinatra-unicorn-ruby.rb
+++ cookbooks/appserver/recipes/sinatra-unicorn-ruby.rb
@@ -14,16 +14,6 @@ execute '/var/chef/config/install_ruby.sh' do
  action :run
 end
 
-#Install gems
-#
-#Path where we install our gems
-gem_path="/usr/local/rvm/rubies/ruby-#{node[:appserver][:ruby][:default_version]}/bin/gem"
-
-gem_package 'unicorn' do
- action :install
- gem_binary(gem_path)
-end
-
 #create app user
 user node[:appserver][:app_user] do
   action :create
@@ -40,19 +30,17 @@ directory node[:appserver][:app_base_dir] do
  recursive true
 end
 
-
-#application directory
-
-directory node[:appserver][:app_dir] do
+# Unicorn config directory
+directory "/etc/unicorn" do
  action :create
- user node[:appserver][:app_user]
  mode 0755
 end
 
 
-git "Rea Sinatra App" do
-  repository node[:appserver][:git_repository]
-  revision node[:appserver][:git_revision]
-  action :sync
-  destination node[:appserver][:app_dir]
+#Unicorn init file
+template "/etc/init.d/unicorn" do
+ source "unicorn.init.erb"
+ action :create
+ mode 0750
 end
+
--- cookbooks/role_sinatra_app/recipes/default.rb
+++ cookbooks/role_sinatra_app/recipes/default.rb
@@ -4,5 +4,68 @@
 #
 #
 include_recipe "common"
-include_recipe "webserver::nginx"
 include_recipe "appserver::sinatra-unicorn-ruby"
+include_recipe "webserver::nginx"
+#unicorn config file for the sinatra app
+app_user=node[:appserver][:app_user]
+app_dir=node[:role_sinatra_app][:app_dir]
+#application directory
+
+directory app_dir do
+ action :create
+ user app_user
+ group app_user
+ mode 0755
+end
+
+git app_dir do
+  repository node[:role_sinatra_app][:git_repository]
+  revision node[:role_sinatra_app][:git_revision]
+  action :sync
+  user app_user
+  group app_user
+end
+
+node[:role_sinatra_app][:dirs].each do |dir_name|
+ directory "#{app_dir}/#{dir_name}" do
+  action :create
+  user app_user
+  group app_user
+  mode 0755
+ end
+end
+
+#unicorn config for this app
+template "#{app_dir}/config/unicorn.rb" do
+ source "unicorn.rb.erb"
+ action :create
+ user app_user
+ group app_user
+ mode 0644
+end
+
+
+# app config files used by unicorn init script
+template "/etc/unicorn/rea.conf" do
+ source "rea.conf.erb"
+ action :create
+ user app_user
+ group app_user
+ mode 0644
+end
+
+template "/var/chef/config/install_app_bundle.sh" do
+ source "install_app_bundle.sh.erb"
+ action :create
+ mode 0755
+ variables(:app_dir => app_dir)
+end
+
+#Install gems from Gemfile if not already installed
+execute '/var/chef/config/install_app_bundle.sh' do
+  action :run
+end
+
+service 'unicorn' do
+ action [:enable, :start]
+end
