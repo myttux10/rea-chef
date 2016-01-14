b66259a90f2946915c822f8041f530b27a78a93c
mbh <myttux10@gmail.com>
2016-01-14 15:11:22 +0930

    add maintainer information in cookbook metatada file

--- cookbooks/common/metadata.rb
+++ cookbooks/common/metadata.rb
@@ -1,5 +1,7 @@
 name             'common'
 license          'GNU Public License 2.0'
 description      'Installs/Configures common'
+maintainer       'mbh'
+maintainer_email 'myttux10@gmail.com'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
